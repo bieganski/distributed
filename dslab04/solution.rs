4978bd6d6f59a3060184c7b2270822a9f3acb8b7
Matuesz <bieganski.m@wp.pl>
2020-11-18 23:52:53 +0100

    working dslab04, need to cleanup

--- dslab04/solution.rs
+++ dslab04/solution.rs
@@ -4,12 +4,10 @@ use std::sync::Arc;
 
 use hmac::{Hmac, NewMac, Mac};
 use sha2::Sha256;
+use std::convert::TryInto;
 
 
 type HmacSha256 = Hmac<Sha256>;
-
-pub static MSG_LEN : usize  = 4;
-pub static HMAC_LEN : usize = 32;
 
 // PhantomData marker is here only to remove `unused type parameter` error.
 // Your final solution should not need it.
@@ -46,11 +44,11 @@ impl<L: Read + Write> SecureClient<L> {
         let mut msg = (data.len() as u32).to_be_bytes().to_vec(); // .to_be(); // u32::from(data.len());
         msg.extend(&data);
         // msg_len.extend();
-        let mut mac = HmacSha256::new_varkey(&[1, 2, 3]).expect("HMAC can take key of any size");
+        let mut mac = HmacSha256::new_varkey(&self.key).expect("HMAC can take key of any size");
         mac.update(msg.as_slice());
         let tag = mac.finalize().into_bytes();
         msg.extend(&tag);
-        // println!("{:?}", tag);
+        println!("t2s2: {:?}", msg);
         // send_data.extend_from_slice(&[0_u8; HMAC_LEN]);
 
 
@@ -84,10 +82,33 @@ impl<L: Read + Write> SecureServer<L> {
 
     /// Returns next unencrypted message with HMAC tag at the end
     pub fn recv_message(&mut self) -> Result<Vec<u8>, SecureServerError> {
-        let mut data  = [0_u8; 50];
+        let mut data = [0_u8; 100];
         // let mut data  = [0_u8; 50].to_vec();
         println!("trying to read data...");
-        self.conn.read(&mut data).unwrap();
+        let total_len : u32 = self.conn.read(&mut data).unwrap() as u32;
+        let msg_size = u32::from_be_bytes((&data[0..4]).try_into().expect("no way"));
+        assert_eq!(msg_size, total_len - 4 - 32); // HMAC tag (32 bytes) + 4 MSG_SIZE (4 bytes)
+        // let msg_size = u32::from_be_bytes(array_ref!(data, 0, 4));
+        
+        // parse message
+        let mut mac = HmacSha256::new_varkey(&self.key).expect("HMAC can take key of any size");
+        mac.update(&data[0..(msg_size as usize)+4]);
+        let tag = mac.finalize().into_bytes();
+
+        let msg_tag = &data[4 + msg_size as usize..4+32+msg_size as usize];
+        // println!("TAGGE: {:?}", tag);
+        // println!("TAGGE: {:?}", &msg_tag);
+
+        let tag_vec = tag.to_vec();
+        let msg_tag_vec = msg_tag.to_vec();
+        println!("TAGGE: {:?}", &tag_vec);
+        println!("TAGGE: {:?}", &msg_tag_vec);
+        if (tag_vec == msg_tag_vec) {
+            print!("TAK!");
+        } else {
+            print!("NIE!");
+        }
+
         // self.conn.read_to_end(&mut data).unwrap(); // TODO inne ready
         // self.conn.read_exact(&mut data).unwrap(); // TODO inne ready
         // self.conn.rea
