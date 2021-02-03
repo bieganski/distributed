505d00ff244e028094372a35968e4bbe442ed537
Matuesz <bieganski.m@wp.pl>
2021-02-03 12:34:57 +0100

    solved dslab12

--- dslab12/solution.rs
+++ dslab12/solution.rs
@@ -4,6 +4,7 @@ use actix::{Actor, Addr, Context, Handler, Message, MessageResult};
 use serde::{Deserialize, Serialize};
 
 use crate::raft::{LogEntry, Raft, ClientRequest};
+use bincode;
 
 pub(crate) struct DistributedSet {
     /// Leader of Raft distributed log.
@@ -19,13 +20,24 @@ impl DistributedSet {
         raft: Addr<Raft>,
         committed_entries: async_channel::Receiver<LogEntry>,
     ) -> Self {
-        unimplemented!()
-        // Start with an empty set.
+
+        Self {
+            raft,
+            committed_entries,
+            integers: HashSet::new(),
+        }
     }
 
     /// Updates the current state of the set by applying the operation.
     fn apply(&mut self, op: SetOperation) {
-        unimplemented!()
+        match op {
+            SetOperation::Add(num) => {
+                self.integers.insert(num);
+            },
+            SetOperation::Remove(num) => {
+                self.integers.remove(&num);
+            }
+        }
     }
 }
 
@@ -48,7 +60,7 @@ impl Handler<SetOperation> for DistributedSet {
     type Result = ();
 
     fn handle(&mut self, msg: SetOperation, _: &mut Self::Context) -> Self::Result {
-        unimplemented!()
+        self.raft.do_send(ClientRequest(bincode::serialize(&msg).unwrap()));
     }
 }
 
@@ -56,6 +68,13 @@ impl Handler<IsPresent> for DistributedSet {
     type Result = MessageResult<IsPresent>;
 
     fn handle(&mut self, msg: IsPresent, _: &mut Self::Context) -> Self::Result {
-        unimplemented!()
+        while let Ok(LogEntry{data, ..}) = self.committed_entries.try_recv() {
+            let cmd : SetOperation = bincode::deserialize(&data).unwrap();
+            self.apply(cmd.clone());
+        }
+        match self.integers.get(&msg.0) {
+            None => MessageResult(false),
+            Some(_) => MessageResult(true),
+        }
     }
 }
